e1ffe8db95e62b247fcef0e59fbe1f79044474e9
cukmekerb <cukmekerb@gmail.com>
2021-10-31 19:04:08 -0700

    stuff is almost working

---
+++ fountain.lua
@@ -0,0 +1,22 @@
+local syntax = require "core.syntax"
+
+syntax.add {
+	files = {"%.fountain$"},
+	patterns = {
+		{ pattern = {"%(", "%)"}, type = "parenthetical" },
+		{ pattern = {"_", "_"}, type = "underline" },
+		{ pattern = {"%*", "%*"}, type = "italic" },
+		{ pattern = {"%*%*", "%*%*"}, type = "bold" },
+		{ regex = "^\\s*[iIeE][nNxX][tT].*", type = "heading" },
+		{ regex = "^\\s*\\..*", type = "heading" },
+		{ regex = "^\\s*[A-Z\\s]+\\s+TO:", type = "transition"},
+		{ regex = "^\\s*>[^<]*$", type = "transition" },
+		-- TODO: character fix
+		{ regex = "^\\s*$^\\s*[A-Z\\s]+\\s*(\\(.*?\\))?\\s*\n(.+\n?)*", type = "literal" },
+		{ regex = "^\\s*@.+?\\n(.+\\n?)*", type = "literal" },
+		-- end todo
+		
+	},
+	symbols = {}
+}
+
---
+++ init.lua
@@ -0,0 +1,50 @@
+-- lite-xl 2.0
+local DocView = require "core.docview"
+local command = require "core.command"
+local core = require "core"
+local style = require "core.style"
+local common = require "core.common"
+
+require "plugins.fountain.fountain"
+
+local ScriptView = DocView:extend()
+
+function ScriptView:draw_line_gutter() return end
+
+
+local old_draw_line_text = ScriptView.draw_line_text
+
+function ScriptView:draw_line_text(idx, x, y)
+  local default_font = self:get_font()
+  local tx, ty = x, y + self:get_line_text_y_offset()
+  for _, type, text in self.doc.highlighter:each_token(idx) do
+    local color = style.syntax[type]
+    local font = style.syntax_fonts[type] or default_font
+    local align = "left"
+    local x_to_use = tx
+    if type == "character" then
+			align = "center"
+			x_to_use = 0
+    end
+    
+    if type == "transition" then
+			align = "right"
+			x_to_use = 0
+    end
+    
+    core.log_quiet(type)
+    tx = common.draw_text(font, color, text, align, x_to_use, ty, self.size.x, self:get_line_height())
+    ::continue::
+  end
+end
+
+local function open_script_view()
+	local node = core.root_view:get_active_node()
+	node:add_view(ScriptView(core.active_view.doc))
+end
+
+command.add(nil, {
+	["script:open"] = open_script_view
+})
+
+return ScriptView
