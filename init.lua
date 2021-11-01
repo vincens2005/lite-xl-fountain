1e8726bcf4f5fbd660fb70a38ffd4921a22a9279
cukmekerb <cukmekerb@gmail.com>
2021-10-31 19:41:22 -0700

    right align works

--- init.lua
+++ init.lua
@@ -17,11 +17,12 @@ local old_draw_line_text = ScriptView.draw_line_text
 function ScriptView:draw_line_text(idx, x, y)
   local default_font = self:get_font()
   local tx, ty = x, y + self:get_line_text_y_offset()
+  local vx, vy = self:get_content_offset()
   for _, type, text in self.doc.highlighter:each_token(idx) do
     local color = style.syntax[type]
     local font = style.syntax_fonts[type] or default_font
     local align = "left"
-    local x_to_use = tx
+    local x_to_use = tx + 1
     if type == "character" then
 			align = "center"
 			x_to_use = 0
@@ -29,11 +30,11 @@ function ScriptView:draw_line_text(idx, x, y)
     
     if type == "transition" then
 			align = "right"
-			x_to_use = 0
+			--x_to_use = 0
     end
     
     core.log_quiet(type)
-    tx = common.draw_text(font, color, text, align, x_to_use, ty, self.size.x, self:get_line_height())
+    tx = common.draw_text(font, color, text, align, x_to_use, ty, self.size.x + vx - style.padding.x * 20, self:get_line_height())
     ::continue::
   end
 end
